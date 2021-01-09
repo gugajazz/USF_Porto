0a9c2dd08b639601c7b68e47ff953388e1e18c9e
HugohGil <74474977+HugohGil@users.noreply.github.com>
2021-01-09 13:26:17 +0000

    Add files via upload

---
+++ USF_Porto-Hugo/js/login.js
@@ -0,0 +1,21 @@
+    var submit_login = document.getElementById("submit_login");
+
+    submit_login.addEventListener("click", function() {
+      var form_conta = document.getElementById("form_conta");
+      if (form_conta.checkValidity()) {
+
+       if (utilizador.value == "exemplo" && palavrapasse.value == "exemplo"){
+         alert("sucesso");
+        }
+       else {         
+          form_conta.classList.add("invalido");
+          form_conta.style.backgroundColor="#FF000022";
+          alert("O nome de utilizador ou palavra-passe foram introduzidos incorretamente.");
+        }
+      }
+    });
+
+
+
+
+
