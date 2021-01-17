a70a4bcccd90139cda6acc2a4c226543f688613b
Gustavo Poçaão <gugapocao@gmail.com>
2021-01-17 18:23:28 +0000

    limpar umas merdas

--- js/slides.js
+++ js/slides.js
@@ -12,15 +12,25 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  var pontos = document.getElementsByClassName("ponto");
+  //slides.length = numero de imagens
+  if (n > slides.length){
+    slideIndex = 1
+  }  //volta ao inicio
+
+  if (n < 1){
+    slideIndex = slides.length
+  } //permite andar do 1o para o ultimo
+
+  for (i = 0; i < slides.length; i++)
+  {
       slides[i].style.display = "none";  
+  }/*apenas um slide visivel de cada vez*/
+
+  for (i = 0; i < pontos.length; i++) {
+      pontos[i].className = pontos[i].className.replace(" active", "");
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
+  
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  pontos[slideIndex-1].className += " active";
 }
