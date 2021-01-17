964565d00980601b15b99f091033e8936b3021ef
Gustavo Poçaão <gugapocao@gmail.com>
2021-01-17 14:27:54 +0000

    apagar piramide.js

--- js/colapsar.js
+++ js/colapsar.js
@@ -1,14 +1,27 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
 
+
+var coll = document.getElementsByClassName("caixa_dropdown");
+//coll.length = numero de caixas
+var i;
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+
+  coll[i].addEventListener("click", 
+
+  function(){ 
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
-    } else {
+    }
+    else {
       content.style.display = "block";
     }
-  });
-}
+  }
+  );
+  
+} 
+
+
+
+
+
